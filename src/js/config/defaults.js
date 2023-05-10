f57f2a9fab17067168a89819c327933d8e73b6f6
Cody Barr <cody.barr@gmail.com>
2023-05-09 22:17:32 -0700

    feat: update svg icon url

--- src/js/config/defaults.js
+++ src/js/config/defaults.js
@@ -61,7 +61,7 @@ const defaults = {
   // Sprite (for icons)
   loadSprite: true,
   iconPrefix: 'plyr',
-  iconUrl: 'https://cdn.plyr.io/3.7.0/plyr.svg',
+  iconUrl: 'https://unpkg.com/@loortv/plyr@3.8.1/dist/plyr.svg',
 
   // Blank video (used to prevent errors on source change)
   blankVideo: 'https://cdn.plyr.io/static/blank.mp4',
